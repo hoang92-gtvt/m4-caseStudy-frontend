441269f59607a0269bbb1b356601e4f833eea577
deepvn98 <deepvn988@gmail.com>
2021-07-11 17:01:04 +0700

    sửa lỗi hiển thij course done

---
+++ Js2/showcourse.js
@@ -0,0 +1,23 @@
+window.onload = function (){
+    getCourse();
+}
+
+function getCourse() {
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:8080/api/course",
+        success: function (data) {
+            console.log(data);
+            let c = `<div id="check1">`;
+            for (let i = 0; i < data.length; i++) {
+                c +=`  
+                                        <div class="N-block"> <span class="d-left">Quận<br>
+    ${data[i].district.name}
+    </span> <font color="#000" size="+1"><a href="#">${data[i].description}</a></font> </div>
+                                        <hr>`;
+            }
+            c+=`</div>`
+            document.getElementById("check1").innerHTML = c;
+        }
+    });
+}
